256c284be56ad1bac3d27fae1d8f87f0ee100d70
Jieun1203 <56375897+Jieun1203@users.noreply.github.com>
2023-08-24 16:59:26 +0900

    Update clinical_data_loader.py

--- clinical_data_loader.py
+++ clinical_data_loader.py
@@ -87,3 +87,84 @@ class ImageDataset(Dataset):
 
             
         return img, target, clinical, p_number
+
+class v_ImageDataset(Dataset):
+    def __init__(self, root, clinical_root, mode, transform=None):
+        super(ImageDataset, self).__init__()
+        
+        img_list = os.listdir(root)
+        filenames = [i for i in img_list if 'png' in i]
+
+                    
+        if mode == 'val':
+            df = pd.read_excel(clinical_root, sheet_name = 'val')
+
+        else:
+            df = pd.read_excel(clinical_root, sheet_name = 'Training')
+
+        self.clinical_root = clinical_root
+        self.filenames = filenames
+        self.root = root
+        self.transform = transform
+        self.df = df
+        
+    def __len__(self):
+        return len(self.filenames)
+    
+    
+    def __getitem__(self, index):
+        img, label, sex, age, smoke, p_number = read_data(os.path.join(self.root, self.filenames[index]), self.df)
+        
+        if self.transform:
+            img = self.transform(img)
+        if label == 0:
+            target = torch.tensor([1, 0])
+        else:
+            target = torch.tensor([0, 1])
+
+        clinical_np = np.array([sex, age, smoke], dtype=np.float32)
+        clinical = torch.from_numpy(clinical_np)        
+
+            
+        return img, target, clinical, p_number
+
+class t_ImageDataset(Dataset):
+    def __init__(self, root, clinical_root, mode, transform=None):
+        super(ImageDataset, self).__init__()
+        
+        img_list = os.listdir(root)
+        filenames = [os.path.join(root, imgs) for imgs in img_list if 'png' in imgs]
+
+                    
+        if mode == 'test':
+            df = pd.read_excel(clinical_root, sheet_name = 'Test')
+
+        else:
+            df = pd.read_excel(clinical_root, sheet_name = 'Training')
+
+        
+        self.clinical_root = clinical_root
+        self.filenames = filenames
+        self.root = root
+        self.transform = transform
+        self.df = df
+        
+    def __len__(self):
+        return len(self.filenames)
+    
+    
+    def __getitem__(self, index):
+        img, label, sex, age, smoke, p_number = read_data(self.filenames[index], self.df)
+        
+        if self.transform:
+            img = self.transform(img)
+        if label == 0:
+            target = torch.tensor([1, 0])
+        else:
+            target = torch.tensor([0, 1])
+
+        clinical_np = np.array([sex, age, smoke], dtype=np.float32)
+        clinical = torch.from_numpy(clinical_np)        
+
+            
+        return img, target, clinical, p_number
